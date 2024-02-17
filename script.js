aa2d12dceee417e0107919d04fcb9b123d855d33
Eaduhc <Eaduhc@hotmail.com>
2024-02-17 11:50:51 +0800

    功能：完成玩一局游戏的功能
    playRound(playerSelection, computerSelection)
    是玩一局游戏，根据玩家选择和电脑选择判断一局的输赢

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 function getComputerChoice() {
     // 随机功能没做，电脑固定出 Rock
-    return "Rock"
+    return "Rock";
+}
+
+// 一局游戏根据玩家选择和电脑选择判断输赢
+function playRound(playerSelection, computerSelection) {
+    // 将玩家选择和电脑选择转为小写
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection == computerSelection) {
+        return "Tie!";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You Lose! Scissors beat Paper"
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You Win! Scissors beat Paper";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You Lose! Rock beats Scissors"
+    }
 }
